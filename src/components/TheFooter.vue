0265e07309841a92d515b42fec804a5601f888e4
Steve Triplett <steve.triplett@swordfish.com.au>
2025-01-27 12:00:57 +1030

    Add props to footer component

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="TheFooter">
     <h3>This is the Footer ...</h3>
+    <h3>{{ aboutUs }}</h3>
+    <h3>{{ contactUs }}</h3>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  aboutUs: String,
+  contactUs: String,
+})
+</script>
 
 <style lang="scss">
 .TheFooter h3 {
