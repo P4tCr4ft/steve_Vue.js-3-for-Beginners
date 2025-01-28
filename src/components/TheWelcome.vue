86bee69c6210ba84af797593781b0c0c73b7a715
Steve Triplett <steve.triplett@swordfish.com.au>
2025-01-28 13:08:23 +1030

    Use reactive array to manage user posts

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,8 +1,33 @@
 <template>
-  <SocialPost username="user 1" userId="UserID1" post="This is post by user 1"></SocialPost>
-  <SocialPost username="user 2" userId="UserID2" post="This is post by user 2"></SocialPost>
+  <SocialPost
+    :username="posts[0].username"
+    :userId="posts[0].userId"
+    :avatarSrc="posts[0].avatarSrc"
+    :post="posts[0].post"
+  ></SocialPost>
+  <SocialPost
+    :username="posts[1].username"
+    :userId="posts[1].userId"
+    :avatarSrc="posts[1].avatarSrc"
+    :post="posts[1].post"
+  ></SocialPost>
 </template>
 
 <script setup lang="ts">
+import { reactive } from 'vue'
 import SocialPost from './molecules/SocialPost.vue'
+const posts = reactive([
+  {
+    username: 'user 1',
+    userId: 'UserID1',
+    avatarSrc: 'https://i.pravatar.cc/40',
+    post: 'This is post by user 1',
+  },
+  {
+    username: 'user 2',
+    userId: 'UserID2',
+    avatarSrc: 'https://i.pravatar.cc/40',
+    post: 'This is post by user 2',
+  },
+])
 </script>
